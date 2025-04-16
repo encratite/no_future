bbaed1fa144f94595b5655c1cd4b61faaa955612
void <git.email.address@googlemail.com>
2025-04-16 04:27:47 +0200

    Removed the regression code

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 from generate_contracts import generate_all_contracts, generate_contract
 from chart import render_chart
 from seasonality import analyze_seasonality
-from regression import perform_regression
 
 def get_date_argument(date_string: str) -> pd.Timestamp:
 	pd.to_datetime(date_string, format="%Y-%m-%d", errors="raise")
@@ -24,19 +23,9 @@ seasonality_help = "Analyze seasonality of the specified symbols\n"
 seasonality_help += "Requires the use of --start, --split and --end"
 group.add_argument("--seasonality", metavar="SYMBOLS", nargs="*", help=seasonality_help)
 
-regression_help = "Perform regression test using the specified symbols\n"
-regression_help += "Requires the use of --start, --split and --end"
-group.add_argument("--regression", metavar="SYMBOLS", nargs="*", help=regression_help)
-
 parser.add_argument("--start", metavar="DATE", type=get_date_argument, help="Restrict data to be read to after this date")
 parser.add_argument("--split", metavar="DATE", type=get_date_argument, help="Date at which to split in-sample and out-of-sample data")
 parser.add_argument("--end", metavar="DATE", type=get_date_argument, help="Read no data after this date")
-
-args, _ = parser.parse_known_args()
-if args.regression is not None:
-	reduction_group = parser.add_mutually_exclusive_group()
-	reduction_group.add_argument("--pca", metavar="FEATURES", type=int, help="Apply PCA dimensionality reduction")
-	reduction_group.add_argument("--select-k-best", metavar="FEATURES", type=int, help="Apply dimensionality reduction using mutual information regression")
 
 args = parser.parse_args()
 if args.generate_all:
@@ -56,15 +45,5 @@ elif args.seasonality is not None:
 	split: pd.Timestamp = args.split
 	end: pd.Timestamp = args.end
 	analyze_seasonality(symbols, start, split, end)
-elif args.regression is not None:
-	assert args.start is not None and args.split is not None and args.end is not None
-	symbols: list[str] = args.regression
-	start: pd.Timestamp = args.start
-	split: pd.Timestamp = args.split
-	end: pd.Timestamp = args.end
-	pca: int | None = args.pca
-	select_k_best: int | None = args.select_k_best
-	assert not pca or not select_k_best
-	perform_regression(symbols, start, split, end, pca, select_k_best)
 else:
 	parser.print_help()
