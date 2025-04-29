a1f38a24ae51fce72fee55a11a8a444b106d331d
void <git.email.address@googlemail.com>
2025-04-29 02:17:00 +0200

    This Z-score stuff is broken as hell

--- pattern.py
+++ pattern.py
@@ -2,8 +2,11 @@ from collections import deque
 from statistics import mean, stdev
 from typing import Callable, Final
 from colorama import Fore, Style
+from functools import reduce
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from common import (
 	read_ohlc_series,
@@ -63,6 +66,7 @@ class Pattern:
 	_function: PatternFunction
 	_features: list[PatternFeatures]
 	_returns: list[float]
+	_return_times: list[pd.Timestamp]
 	_unmatched_returns: list[float]
 
 	def __init__(self, name: str, function: PatternFunction) -> None:
@@ -70,6 +74,7 @@ class Pattern:
 		self._function = function
 		self._features = []
 		self._returns = []
+		self._return_times = []
 		self._unmatched_returns = []
 
 	def process(self, i: int, records: list[OhlcRecord], buffers: ZScoreBuffers) -> None:
@@ -78,10 +83,13 @@ class Pattern:
 		local_records = records[offset - limit: offset]
 		features = PatternFeatures(buffers)
 		match = self._function(local_records, features)
-		returns = get_rate_of_change(records[i + 1].close, records[i].close)
+		tomorrow = records[i + 1]
+		today = records[i]
+		returns = get_rate_of_change(tomorrow.close, today.close)
 		if match:
 			self._features.append(features)
 			self._returns.append(returns)
+			self._return_times.append(today.time)
 		else:
 			self._unmatched_returns.append(returns)
 
@@ -98,8 +106,22 @@ class Pattern:
 			hit_rate = 1 - hit_rate
 		return hit_rate
 
-	def get_prevalence(self) -> float:
-		return len(self._features) / (len(self._features) + len(self._unmatched_returns))
+	def get_prevalence(self) -> tuple[float, int]:
+		trades = len(self._features)
+		prevalence = trades / (trades + len(self._unmatched_returns))
+		return prevalence, trades
+
+	def get_equity_curve(self) -> pd.DataFrame:
+		equity = []
+		for returns in self._returns:
+			value = equity[-1] if len(equity) > 0 else 1
+			new_value = value * (1 + returns)
+			equity.append(new_value)
+		df = pd.DataFrame({
+			"time": self._return_times,
+			self.name: equity
+		})
+		return df
 
 def analyze_pattern(symbol: str, start: pd.Timestamp, end: pd.Timestamp) -> None:
 	assert start < end
@@ -108,30 +130,29 @@ def analyze_pattern(symbol: str, start: pd.Timestamp, end: pd.Timestamp) -> None
 		Pattern("-momentum2", lambda ohlc, features: features.momentum2_zscore < -1.5),
 		Pattern("momentum2, momentum3", lambda ohlc, features: features.momentum2_zscore > 1.5 and features.momentum3_zscore > 1.5),
 		Pattern("-momentum2, -momentum3", lambda ohlc, features: features.momentum2_zscore < -1.5 and features.momentum3_zscore < -1.5),
-		Pattern("momentum2, momentum3, momentum5", lambda ohlc, features: features.momentum2_zscore > 1 and features.momentum3_zscore > 0.5 and features.momentum5_zscore > 0.5),
-		Pattern("-momentum2, -momentum3, -momentum5", lambda ohlc, features: features.momentum2_zscore < -1 and features.momentum3_zscore < -0.5 and features.momentum5_zscore < -0.5),
+		# Pattern("momentum2, momentum3, momentum5", lambda ohlc, features: features.momentum2_zscore > 1 and features.momentum3_zscore > 0.5 and features.momentum5_zscore > 0.5),
+		# Pattern("-momentum2, -momentum3, -momentum5", lambda ohlc, features: features.momentum2_zscore < -1 and features.momentum3_zscore < -0.5 and features.momentum5_zscore < -0.5),
 		Pattern("011, momentum2", lambda ohlc, features: ohlc[0].close > ohlc[1].close < ohlc[2].close and features.momentum2_zscore > 1.5),
 		Pattern("100, -momentum2", lambda ohlc, features: ohlc[0].close < ohlc[1].close > ohlc[2].close and features.momentum2_zscore < -1.5),
 		Pattern("011, high, momentum2", lambda ohlc, features: ohlc[0].close > ohlc[1].close < ohlc[2].close and ohlc[0].high < ohlc[2].high and ohlc[1].high < ohlc[2].high and features.momentum2_zscore > 1.5),
 		Pattern("100, low, -momentum2", lambda ohlc, features: ohlc[0].close < ohlc[1].close > ohlc[2].close and ohlc[0].low > ohlc[2].low and ohlc[1].low > ohlc[2].low and features.momentum2_zscore < -1.5),
-		Pattern("000, momentum2", lambda ohlc, features: ohlc[0].close > ohlc[1].close > ohlc[2].close and features.momentum2_zscore < -1),
+		Pattern("000, -momentum2", lambda ohlc, features: ohlc[0].close > ohlc[1].close > ohlc[2].close and features.momentum2_zscore < -1),
 		Pattern("111, momentum2", lambda ohlc, features: ohlc[0].close < ohlc[1].close < ohlc[2].close and features.momentum2_zscore > 1),
-		Pattern("000, momentum3", lambda ohlc, features: ohlc[0].close > ohlc[1].close > ohlc[2].close and features.momentum3_zscore < -1),
-		Pattern("111, momentum3", lambda ohlc, features: ohlc[0].close < ohlc[1].close < ohlc[2].close and features.momentum3_zscore > 1),
-		Pattern("000, momentum5", lambda ohlc, features: ohlc[0].close > ohlc[1].close > ohlc[2].close and features.momentum5_zscore < -1),
-		Pattern("111, momentum5", lambda ohlc, features: ohlc[0].close < ohlc[1].close < ohlc[2].close and features.momentum5_zscore > 1),
-		Pattern("000, momentum10", lambda ohlc, features: ohlc[0].close > ohlc[1].close > ohlc[2].close and features.momentum10_zscore < -1),
-		Pattern("111, momentum10", lambda ohlc, features: ohlc[0].close < ohlc[1].close < ohlc[2].close and features.momentum10_zscore > 1),
-		Pattern("000, channel", lambda ohlc, features: ohlc[0].close > ohlc[1].close > ohlc[2].close and ohlc[0].low > ohlc[1].low > ohlc[2].low and ohlc[0].high > ohlc[1].high > ohlc[2].high and features.momentum2_zscore < -1),
-		Pattern("111, -channel", lambda ohlc, features: ohlc[0].close < ohlc[1].close < ohlc[2].close and ohlc[0].low < ohlc[1].low < ohlc[2].low and ohlc[0].high < ohlc[1].high < ohlc[2].high and features.momentum2_zscore > 1),
+		# Pattern("000, -momentum3", lambda ohlc, features: ohlc[0].close > ohlc[1].close > ohlc[2].close and features.momentum3_zscore < -1),
+		# Pattern("111, momentum3", lambda ohlc, features: ohlc[0].close < ohlc[1].close < ohlc[2].close and features.momentum3_zscore > 1),
+		# Pattern("000, -momentum5", lambda ohlc, features: ohlc[0].close > ohlc[1].close > ohlc[2].close and features.momentum5_zscore < -1),
+		# Pattern("111, momentum5", lambda ohlc, features: ohlc[0].close < ohlc[1].close < ohlc[2].close and features.momentum5_zscore > 1),
+		# Pattern("000, -momentum10", lambda ohlc, features: ohlc[0].close > ohlc[1].close > ohlc[2].close and features.momentum10_zscore < -1),
+		# Pattern("111, momentum10", lambda ohlc, features: ohlc[0].close < ohlc[1].close < ohlc[2].close and features.momentum10_zscore > 1),
+		Pattern("000, channel", lambda ohlc, features: ohlc[0].close > ohlc[1].close > ohlc[2].close and ohlc[0].low > ohlc[1].low > ohlc[2].low and ohlc[0].high > ohlc[1].high > ohlc[2].high),
+		Pattern("111, -channel", lambda ohlc, features: ohlc[0].close < ohlc[1].close < ohlc[2].close and ohlc[0].low < ohlc[1].low < ohlc[2].low and ohlc[0].high < ohlc[1].high < ohlc[2].high),
 	]
 	series = read_ohlc_series(symbol)
 	records: list[OhlcRecord] = series.values()
 	buffers = ZScoreBuffers()
-	past_offset = pd.Timedelta(days=10 + 1)
+	past_offset = pd.DateOffset(years=1)
 	for i, record in enumerate(records):
-		lookback = record.time - past_offset
-		if record.time < lookback or Strategy.is_banned_symbol(symbol, record.time) or Strategy.is_banned_symbol(symbol, lookback):
+		if record.time < start - past_offset:
 			continue
 		momentum2 = get_momentum(2, i, records)
 		momentum3 = get_momentum(3, i, records)
@@ -141,11 +162,37 @@ def analyze_pattern(symbol: str, start: pd.Timestamp, end: pd.Timestamp) -> None
 		buffers.momentum3.add(momentum3)
 		buffers.momentum5.add(momentum5)
 		buffers.momentum10.add(momentum10)
-		if record.time < start or record.time >= end:
+		if record.time < start:
 			continue
+		if record.time >= end:
+			break
 		for pattern in patterns:
 			pattern.process(i, records, buffers)
 	print_statistics(patterns)
+	render_returns(patterns)
+
+def render_returns(patterns: list[Pattern]) -> None:
+	id_var = "time"
+	var_name = "Strategy"
+	value_name = "value_name"
+	pattern_dfs = [x.get_equity_curve() for x in patterns]
+	merged_df = reduce(lambda left, right: pd.merge(left, right, on=id_var, how="outer"), pattern_dfs)
+	names = [x.name for x in patterns]
+	melted_df = merged_df.melt(
+		id_vars=id_var,
+		value_vars=names,
+		var_name=var_name,
+		value_name=value_name
+	)
+	plt.figure(figsize=(12, 8))
+	sns.lineplot(melted_df, x=id_var, y=value_name, hue=var_name)
+	plt.xlim(melted_df[id_var].min(), melted_df[id_var].max())
+	plt.xlabel("Time")
+	plt.ylabel("Equity")
+	plt.title(f"Equity Curve")
+	plt.tight_layout()
+	plt.show()
+	plt.close()
 
 def print_statistics(patterns: list[Pattern]) -> None:
 	headers = [
@@ -153,11 +200,12 @@ def print_statistics(patterns: list[Pattern]) -> None:
 		"Return (Pattern)",
 		"Return (Others)",
 		"Hit Rate",
-		"Prevalence"
+		"Prevalence",
+		"Trades"
 	]
 	table = [headers]
 	for pattern in patterns:
-		prevalence = pattern.get_prevalence()
+		prevalence, trades = pattern.get_prevalence()
 		if prevalence > 0.02:
 			prevalence_string = f"{prevalence:.2%}"
 		else:
@@ -177,7 +225,8 @@ def print_statistics(patterns: list[Pattern]) -> None:
 				format_percentage(mean_return_pattern),
 				format_percentage(mean_return_others),
 				hit_rate_string,
-				prevalence_string
+				prevalence_string,
+				trades
 			]
 		else:
 			row = [
@@ -185,7 +234,8 @@ def print_statistics(patterns: list[Pattern]) -> None:
 				"-",
 				"-",
 				"-",
-				prevalence_string
+				prevalence_string,
+				trades
 			]
 		table.append(row)
 	print_table(table)
